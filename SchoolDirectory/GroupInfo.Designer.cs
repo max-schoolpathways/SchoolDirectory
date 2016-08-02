5e1c296a24edd201319a7c06ddf094633216cfe3
max-schoolpathways <max.storyforge@gmail.com>
2016-08-02 13:57:31 -0700

    minor tweaks

--- SchoolDirectory/GroupInfo.Designer.cs
+++ SchoolDirectory/GroupInfo.Designer.cs
@@ -35,16 +35,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.primaryContactName = new System.Windows.Forms.Label();
+            this.directorName = new System.Windows.Forms.Label();
             this.saveButton = new System.Windows.Forms.Button();
             this.deleteButton = new System.Windows.Forms.Button();
             this.removeProductButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupNameBox = new System.Windows.Forms.TextBox();
+            this.scopeNameBox = new System.Windows.Forms.TextBox();
+            this.noteTextbox = new System.Windows.Forms.RichTextBox();
+            this.productsList = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -64,14 +64,14 @@
             this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.linkLabel2, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.primaryContactName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.directorName, 3, 1);
             this.tableLayoutPanel1.Controls.Add(this.saveButton, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.deleteButton, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.removeProductButton, 3, 3);
             this.tableLayoutPanel1.Controls.Add(this.button1, 2, 3);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupNameBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.scopeNameBox, 3, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -86,7 +86,7 @@
             // groupBox1
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 2);
-            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.noteTextbox);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(3, 195);
             this.groupBox1.Name = "groupBox1";
@@ -98,7 +98,7 @@
             // groupBox2
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.groupBox2, 2);
-            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Controls.Add(this.productsList);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(212, 195);
             this.groupBox2.Name = "groupBox2";
@@ -149,25 +149,25 @@
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "Group Director";
             // 
-            // label3
+            // primaryContactName
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(106, 153);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 26);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Primary Contact Name";
+            this.primaryContactName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.primaryContactName.AutoSize = true;
+            this.primaryContactName.Location = new System.Drawing.Point(106, 153);
+            this.primaryContactName.Name = "primaryContactName";
+            this.primaryContactName.Size = new System.Drawing.Size(100, 26);
+            this.primaryContactName.TabIndex = 6;
+            this.primaryContactName.Text = "Primary Contact Name";
             // 
-            // label4
+            // directorName
             // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(317, 153);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 26);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Group Director Name";
+            this.directorName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.directorName.AutoSize = true;
+            this.directorName.Location = new System.Drawing.Point(317, 153);
+            this.directorName.Name = "directorName";
+            this.directorName.Size = new System.Drawing.Size(104, 26);
+            this.directorName.TabIndex = 7;
+            this.directorName.Text = "Group Director Name";
             // 
             // saveButton
             // 
@@ -209,39 +209,40 @@
             this.button1.Text = "Add Product";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // groupNameBox
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox1.Location = new System.Drawing.Point(106, 60);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 12;
+            this.groupNameBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.groupNameBox.Location = new System.Drawing.Point(106, 60);
+            this.groupNameBox.Name = "groupNameBox";
+            this.groupNameBox.Size = new System.Drawing.Size(100, 20);
+            this.groupNameBox.TabIndex = 12;
             // 
-            // textBox2
+            // scopeNameBox
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox2.Location = new System.Drawing.Point(317, 60);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 13;
+            this.scopeNameBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.scopeNameBox.Location = new System.Drawing.Point(317, 60);
+            this.scopeNameBox.Name = "scopeNameBox";
+            this.scopeNameBox.Size = new System.Drawing.Size(100, 20);
+            this.scopeNameBox.TabIndex = 13;
             // 
-            // richTextBox1
+            // noteTextbox
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(197, 142);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.noteTextbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.noteTextbox.Location = new System.Drawing.Point(3, 16);
+            this.noteTextbox.Name = "noteTextbox";
+            this.noteTextbox.Size = new System.Drawing.Size(197, 142);
+            this.noteTextbox.TabIndex = 0;
+            this.noteTextbox.Text = "";
             // 
-            // richTextBox2
+            // productsList
             // 
-            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox2.Location = new System.Drawing.Point(3, 16);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(203, 142);
-            this.richTextBox2.TabIndex = 1;
-            this.richTextBox2.Text = "";
+            this.productsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.productsList.FormattingEnabled = true;
+            this.productsList.Location = new System.Drawing.Point(3, 16);
+            this.productsList.Name = "productsList";
+            this.productsList.Size = new System.Drawing.Size(203, 142);
+            this.productsList.Sorted = true;
+            this.productsList.TabIndex = 0;
             // 
             // GroupInfo
             // 
@@ -268,15 +269,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.LinkLabel linkLabel2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label primaryContactName;
+        private System.Windows.Forms.Label directorName;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Button removeProductButton;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox noteTextbox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox groupNameBox;
+        private System.Windows.Forms.TextBox scopeNameBox;
+        private System.Windows.Forms.ListBox productsList;
     }
 }
