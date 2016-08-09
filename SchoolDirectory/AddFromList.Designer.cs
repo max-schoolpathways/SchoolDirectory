8ef85039bb5d8a253b58b81ca3d7590d2dff7d2b
max-schoolpathways <max.storyforge@gmail.com>
2016-08-08 17:00:11 -0700

    -created "AddFromList" form

---
+++ SchoolDirectory/AddFromList.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SchoolDirectory
+{
+    partial class AddFromList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.selectButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.68156F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.31844F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(205, 179);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // selectButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.selectButton, 2);
+            this.selectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selectButton.Location = new System.Drawing.Point(3, 151);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(199, 25);
+            this.selectButton.TabIndex = 0;
+            this.selectButton.Text = "Select";
+            this.selectButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.listBox, 2);
+            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(3, 3);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(199, 142);
+            this.listBox.TabIndex = 1;
+            // 
+            // AddFromList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 179);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "AddFromList";
+            this.Text = "AddFromList";
+            this.Load += new System.EventHandler(this.AddFromList_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
---
+++ SchoolDirectory/AddFromList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SchoolClasses;
+
+namespace SchoolDirectory
+{
+    public partial class AddFromList : Form
+    {
+        public enum addModeEnum {Product};
+        public addModeEnum addMode;
+        private BindingSource listSource = new BindingSource();
+        
+        public AddFromList()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFromList_Load(object sender, EventArgs e)
+        {
+            if(addMode == addModeEnum.Product)
+            {
+                listSource.DataSource = ProductEntry.allProducts;
+            }
+            listBox.DataSource = listSource;
+            listSource.ResetBindings(false);
+            
+        }
+    }
+}
